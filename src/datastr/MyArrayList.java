c817d68de81e33dea1717317da2f0b4079a8489d
Ritvars <ritvarsbaders@gmail.com>
2026-02-19 11:37:33 +0200

    Add: added variables in ArrayList class

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,5 +1,15 @@
 package datastr;
 
 public class MyArrayList {
-
+	//mainigie
+	private char[] list;
+	private int how_many_elements = 0;
+	private final int DEFAULT_SIZE = 8;
+	private int size = DEFAULT_SIZE;
+	
+	//konstruktori
+	
+	
+	//funkcijas
+		
 }
